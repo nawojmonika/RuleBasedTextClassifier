3e47caa396d4fac27669704175014ff4f476b70f
grayrattus <arturdziedziczak0@gmail.com>
2019-03-21 19:23:15 +0100

    Add load of whole directory with reut2

--- src/main/java/Sztuczna/FXMLController.java
+++ src/main/java/Sztuczna/FXMLController.java
@@ -8,26 +8,35 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.FileChooser;
 
 public class FXMLController implements Initializable {
-    
+
     @FXML
     private Label label;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
-        FileChooser fileChooser = new FileChooser();
-        File selectedFile = fileChooser.showOpenDialog(null);
+        DirectoryChooser fileChooser = new DirectoryChooser();
+        File selectedFolder = fileChooser.showDialog(null);
 
         Loader l = new Loader();
-        ArrayList<Article> articles = l.loadFile(selectedFile);
+        ArrayList<Article> articles = new ArrayList<>();
+        for (final File fileEntry : selectedFolder.listFiles()) {
+            if (fileEntry.isFile()) {
+                if (fileEntry.getName().contains("reut2")) {
+                    articles.addAll(l.loadFile(fileEntry));
+                };
+            }
+        }
+        System.out.println(articles.toString());
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 }
