3cc12ff7333fe2a1a962cdc67f5f46bc4f5ee63c
nawojmonika <nawojmonika@gmail.com>
2019-04-11 21:37:04 +0200

    Added extended n gram metric (Niewiadomski metric)

---
+++ src/main/java/Sztuczna/Metrics/ExtendedNGramSimilarity.java
@@ -0,0 +1,17 @@
+package Sztuczna.Metrics;
+
+public class ExtendedNGramSimilarity implements TextSimilarityMetric {
+    public Double calculateSimilarity(String prop1, String prop2) {
+        int N = Math.max(prop1.length(), prop2.length());
+        Double sum = 0.0;
+        prop1.toLowerCase();
+        prop2.toLowerCase();
+        for (int i = 0; i < N ; i++) {
+            for (int j = 0; j < N - i + 1 ; j++) {
+                sum += prop1.substring(i, i + j) == prop2.substring(i, i + j) ? 1 : 0;
+            }
+        }
+        Double sim = (2 / Math.pow(N,2) + N) * sum;
+        return sim;
+    }
+}
