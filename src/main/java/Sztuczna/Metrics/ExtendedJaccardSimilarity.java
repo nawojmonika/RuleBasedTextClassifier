20e8aabe44fc43fad84ab44b8fc475aa286e898b
nawojmonika <nawojmonika@gmail.com>
2019-04-13 19:39:27 +0200

    Added Jaccard similarity metric

--- src/main/java/Sztuczna/Metrics/ExtendedJaccardSimilarity.java
+++ src/main/java/Sztuczna/Metrics/ExtendedJaccardSimilarity.java
@@ -2,16 +2,24 @@ package Sztuczna.Metrics;
 
 public class ExtendedJaccardSimilarity implements TextSimilarityMetric {
     public Double calculateSimilarity(String prop1, String prop2) {
+        int n = 2; // because text is in english
         int N = Math.max(prop1.length(), prop2.length());
-        Double sum = 0.0;
+        Double common = 0.0;
+        Double unshared = 0.0;
         prop1.toLowerCase();
         prop2.toLowerCase();
         for (int i = 0; i < N ; i++) {
             for (int j = 0; j < N - i + 1 ; j++) {
-                sum += prop1.substring(i, i + j) == prop2.substring(i, i + j) ? 1 : 0;
+                String substring = prop1.substring(i, i + n);
+                if(prop2.indexOf(substring) != -1){
+                    common += 1;
+                }
+                else {
+                    unshared += 1;
+                }
             }
         }
-        Double sim = (2 / Math.pow(N,2) + N) * sum;
+        Double sim = (common + unshared) / N;
         return sim;
     }
 }
