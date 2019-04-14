0de8686be5d9c28ea0b4a64ffbd834b3f7b99dd3
nawojmonika <nawojmonika@gmail.com>
2019-04-14 13:05:06 +0200

    Updated metrics by getting difference value from abstract class

--- src/main/java/Sztuczna/Metrics/ChebyshevMetric.java
+++ src/main/java/Sztuczna/Metrics/ChebyshevMetric.java
@@ -8,16 +8,17 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class ChebyshevMetric extends HandleDifference implements Metric {
-    public Double calculateDistance(Set<Property> set1, Set<Property> set2) {
+    public Double calculateDistance(Set<Property> set1, Set<Property> set2, TextSimilarityMetric metric) {
         ArrayList<Double> distances = new ArrayList<Double>();
         Iterator<Property> p1Iterator = set1.iterator();
         Iterator<Property> p2Iterator = set2.iterator();
 
 
         while (p1Iterator.hasNext() && p2Iterator.hasNext()) {
-            Double x = p1Iterator.next().getValue().doubleValue();
-            Double y = p2Iterator.next().getValue().doubleValue();
-            distances.add(Math.abs(x- y));
+            Property prop1 = p1Iterator.next();
+            Property prop2 = p2Iterator.next();
+            Double diff = this.getDifference(prop1, prop2, metric);
+            distances.add(Math.abs(diff));
         }
 
         return Collections.max(distances);
--- src/main/java/Sztuczna/Metrics/ManhattanMetric.java
+++ src/main/java/Sztuczna/Metrics/ManhattanMetric.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 import java.util.Set;
 
 public class ManhattanMetric extends HandleDifference implements Metric{
-    public Double calculateDistance(Set<Property> set1, Set<Property> set2) {
+    public Double calculateDistance(Set<Property> set1, Set<Property> set2, TextSimilarityMetric metric) {
         Double dist = 0.0;
 
         Iterator<Property> p1Iterator = set1.iterator();
@@ -14,9 +14,10 @@ public class ManhattanMetric extends HandleDifference implements Metric{
 
 
         while (p1Iterator.hasNext() && p2Iterator.hasNext()) {
-            Double x = p1Iterator.next().getValue().doubleValue();
-            Double y = p2Iterator.next().getValue().doubleValue();
-            dist += Math.abs(x - y);
+            Property prop1 = p1Iterator.next();
+            Property prop2 = p2Iterator.next();
+            Double diff = this.getDifference(prop1, prop2, metric);
+            dist += Math.abs(diff);
         }
         return dist;
     }
